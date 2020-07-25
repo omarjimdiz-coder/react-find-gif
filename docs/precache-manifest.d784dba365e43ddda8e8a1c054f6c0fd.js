ef04a52b4932cf57d9eefc7fb0475cb192ed5768
Omar <omartck1620@gmail.com>
2020-07-24 20:58:43 -0500

    Se agregó docs

---
+++ docs/precache-manifest.d784dba365e43ddda8e8a1c054f6c0fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2e5dec4f12400ead3da63082349fef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c76f6cbc52c11b90ff61",
+    "url": "/static/css/main.62515dca.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c76f6cbc52c11b90ff61",
+    "url": "/static/js/main.9e8a6213.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
